4cf8b1a4485d94ef826bb5e7343ec94a05827de9
bentlyy <garayaa0606@gmail.com>
2026-05-05 16:57:56 -0400

    style: soluciones de interfaz

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
+import hpp from 'hpp';
 import 'dotenv/config';
 import { fileURLToPath } from 'url';
 import { dirname, resolve } from 'path';
