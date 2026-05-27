5d02ca453704594a32c5e42dc1ab0ce7bac16794
bentlyy <garayaa0606@gmail.com>
2026-05-27 19:18:51 -0400

    fix: rejectUnauthorized false para PostgreSQL en Render

--- src/shared/db.ts
+++ src/shared/db.ts
@@ -15,7 +15,7 @@ const isInternalDb = (): boolean => {
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: !isInternalDb() && process.env.NODE_ENV === 'production' ? { rejectUnauthorized: true } : false,
+  ssl: !isInternalDb() && process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 5000,
