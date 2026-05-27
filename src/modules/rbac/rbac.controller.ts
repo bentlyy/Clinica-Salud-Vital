8146b2b050155e5ec88d7cc797540390a2b00dbd
bentlyy <garayaa0606@gmail.com>
2026-05-27 19:00:18 -0400

    fix: TS7006 implicit any en ml.service, rbac, specialties, db, email

--- src/modules/rbac/rbac.controller.ts
+++ src/modules/rbac/rbac.controller.ts
@@ -1,7 +1,8 @@
+import { Request, Response } from 'express';
 import { getUserPermissions } from './rbac.service.js';
 import { asyncHandler } from '../../middlewares/asyncHandler.middleware.js';
 
-export const getMyPermissions = asyncHandler(async (req, res) => {
+export const getMyPermissions = asyncHandler(async (req: Request, res: Response) => {
   const permissions = await getUserPermissions(req.user!.id, req.user!.role, req.tenant_id);
   res.json({ role: req.user!.role, permissions });
 });
